2955935ca51ec6c60429e8027708c2a6cf0cb0be
Daniel Yang <danielyang@daniels-mini.lan>
2014-05-20 18:23:37 +0800

    Initial Commit

---
+++ myapp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  myapp
+//
+//  Created by Daniel Yang on 5/20/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ myapp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myapp
+//
+//  Created by Daniel Yang on 5/20/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
